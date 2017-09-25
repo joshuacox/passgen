cfb881584a7f5942577342d54f8b25fd9044fa31
Josh Cox <josh@webhosting.coop>
2017-09-25 10:49:06 -0500

    exports

--- tester.sh
+++ tester.sh
@@ -4,6 +4,14 @@ THRESH=$2
 IFS=','
 while read zoro Filebytes Entropy ChiSquare Mean MonteCarloPi SerialCorrelation; do
  # echo "$zoro $Filebytes $Entropy $ChiSquare $Mean $MonteCarloPi $SerialCorrelation"
+export zoro=$zoro
+export Filebytes=$Filebytes
+export Entropy=$Entropy
+export ChiSquare=$ChiSquare
+export Mean=$Mean
+export MonteCarloPi=$MonteCarloPi
+
+SerialCorrelation"
  if ! [[ $(echo "$Entropy > $THRESH" | bc) -gt "0" ]] ; then
    echo -n "Entropy too low at"
    echo $Entropy
